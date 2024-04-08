cf0484f49c1956ac576884d811bd2ec6e23a1f6e
Clay Mangum <clay@trinitasaccounting.com>
2024-04-08 17:46:56 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -47,6 +47,39 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        
+        if plant:
+            for attr in request.get_json():
+                setattr(plant, attr, request.get_json()[attr])
+            db.session.add(plant)
+            db.session.commit()
+
+            response_dict = plant.to_dict()
+
+            response = make_response(response_dict, 200)
+
+            return response
+
+        else:
+            response_body = {}
+
+            return make_response(response_body, 404)
+
+
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_dict = {}
+
+        response = make_response(response_dict, 204)
+
+        return response
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
